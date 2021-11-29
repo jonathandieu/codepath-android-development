600b36d0dce6503b7875e4b80072ad18f66813bd
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-11-29 01:59:39 -0500

    Implemented adding items

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.simpletodo.databinding.ActivityMainBinding
@@ -23,10 +24,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // 1. Detect when the user clicks on the add button
-        findViewById<Button>(R.id.button).setOnClickListener {
-            // Any code written here will be executed when user clicks on a button
-            Log.i("Jon", "User clicked on button")
-        }
+//        findViewById<Button>(R.id.button).setOnClickListener {
+//            // Any code written here will be executed when user clicks on a button
+//            Log.i("Jon", "User clicked on button")
+//        }
         listOfTasks.add("Do laundry")
         listOfTasks.add("Go for walk")
 
@@ -37,6 +38,24 @@ class MainActivity : AppCompatActivity() {
         // Attach the adapter to the recyclerview to populate items
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Set up the button and input field so that the user can enter a task and add it to the list
+        val inputTextField = findViewById<EditText>(R.id.addTaskField)
+
+        // Get a reference to the button and set an onclickListener
+        findViewById<Button>(R.id.button).setOnClickListener {
+            // 1. Grab the text the user has inputted
+            val userInputtedTask = inputTextField.text.toString()
+
+            // 2. Add the string to our list of tasks: listofTasks
+            listOfTasks.add(userInputtedTask)
+
+            // Notify the adapter that the data has been updated
+            adapter.notifyItemInserted(listOfTasks.size - 1)
+
+            // 3. Reset the text field so that it autoclears on every tasks
+            inputTextField.setText("")
+        }
 
 
 
