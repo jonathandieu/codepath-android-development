2a45520c9daf7f6c47a684fd2220794c42215f6d
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-11-29 01:51:27 -0500

    Implemented rendering of list of items

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.simpletodo
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,50 +10,35 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.simpletodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val listOfTasks = mutableListOf<String>()
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        // 1. Detect when the user clicks on the add button
+        findViewById<Button>(R.id.button).setOnClickListener {
+            // Any code written here will be executed when user clicks on a button
+            Log.i("Jon", "User clicked on button")
         }
-    }
+        listOfTasks.add("Do laundry")
+        listOfTasks.add("Go for walk")
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+        // Look up recyclerView in layout
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        // Create adapter passing in the sample user data
+        val adapter = TaskItemAdaptor(listOfTasks)
+        // Attach the adapter to the recyclerview to populate items
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+
     }
 }
---
+++ app/src/main/java/com/example/simpletodo/TaskItemAdaptor.kt
@@ -0,0 +1,45 @@
+package com.example.simpletodo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+//  Bridge that tells teh recyclerView how to display the data we give it
+class TaskItemAdaptor(val listOfItems: List<String>) : RecyclerView.Adapter<TaskItemAdaptor.ViewHolder>() {
+
+    // Usually involves inflating a layout from XML and return the holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val item = listOfItems.get(position)
+
+        holder.textView.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listOfItems.size
+    }
+
+    // Provide direct reference to each of the views within data item
+    // Used to cache the views within the item layout for fast access
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // TODO
+        // Populate data by grabbing references to the strings in our app
+        // Store references
+        val textView: TextView
+
+        init {
+            textView = itemView.findViewById(android.R.id.text1)
+        }
+    }
+}
